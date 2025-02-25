c18d90664dcc82c9d44b8f362726071fa65aeb50
hennayasmine19 <hennayasmineshaik@gmail.com>
2025-02-25 21:02:23 +0530

    2

--- client/app.js
+++ client/app.js
@@ -1,29 +1,29 @@
 function getBathValue() {
     var uiBathrooms = document.getElementsByName("uiBathrooms");
-    for(var i in uiBathrooms) {
-      if(uiBathrooms[i].checked) {
-          return parseInt(i)+1;
-      }
-    }
-    return -1; // Invalid Value
-  }
-  
-function getBHKValue() {
-    var uiBHK = document.getElementsByName("uiBHK");
-    for(var i in uiBHK) {
-        if(uiBHK[i].checked) {
-            return parseInt(i)+1;
+    for (var i = 0; i < uiBathrooms.length; i++) {
+        if (uiBathrooms[i].checked) {
+            return parseInt(uiBathrooms[i].value);
         }
     }
     return -1; // Invalid Value
-  }
-  
-  function onClickedEstimatePrice() {
+}
+
+function getBHKValue() {
+    var uiBHK = document.getElementsByName("uiBHK");
+    for (var i = 0; i < uiBHK.length; i++) {
+        if (uiBHK[i].checked) {
+            return parseInt(uiBHK[i].value);
+        }
+    }
+    return -1; // Invalid Value
+}
+
+function onClickedEstimatePrice() {
     var sqft = document.getElementById("uiSqft").value;
     var bhk = getBHKValue();
     var bathrooms = getBathValue();
     var location = document.getElementById("uiLocations").value;
-    
+
     var url = "http://127.0.0.1:5000/predict_home_price"; // Correct Flask server endpoint
 
     $.post(url, {
@@ -31,28 +31,27 @@ function getBHKValue() {
         bhk: bhk,
         bath: bathrooms,
         location: location
-    }, function(data, status) {
+    }, function (data, status) {
         console.log(data.estimated_price);
         document.getElementById("uiEstimatedPrice").innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
     });
 }
 
-  
 function onPageLoad() {
-    console.log( "document loaded" );
+    console.log("document loaded");
     var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
-    $.get(url,function(data, status) {
+    $.get(url, function (data, status) {
         console.log("got response for get_location_names request");
-        if(data) {
+        if (data) {
             var locations = data.locations;
             var uiLocations = document.getElementById("uiLocations");
             $('#uiLocations').empty();
-            for(var i in locations) {
+            for (var i = 0; i < locations.length; i++) {
                 var opt = new Option(locations[i]);
                 $('#uiLocations').append(opt);
             }
         }
     });
-  }
-  
-  window.onload = onPageLoad;
+}
+
+window.onload = onPageLoad;
